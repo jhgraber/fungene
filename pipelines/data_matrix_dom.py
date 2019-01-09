b892fea4c04dc26c12cbdf6f699ea2c738ce658f
Lucie Hutchins <LucyHut@users.noreply.github.com>
2019-01-09 16:06:13 -0500

    refactoring

--- pipelines/data_matrix_dom.py
+++ pipelines/data_matrix_dom.py
@@ -1,46 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-A data model for the input file
-Field: defines data model for a cell/field within a row
-   Args: the index of the field
-   Members: index, value
-Row: defined a data model for a row within a file
-   Args: a line, a field delimiter
-   Members: array of fields
-
-FileDOM: defines a data model for the file 
-   Args: file_name
-   Members: header , file_name
-
+A data model for data matrix
+Columns: a vector of columns header
+Rows: a vector of [target column - row id]=>vector of values
+   Args: a row values, row ID
+   Members: row_target_id, vector of values
 Returns:
      Sets global  
 Raises:
      Nothing
 """
 import os
-from os.path import isfile
+from file_dom import FileDOM
 
-class Field:
-    def __init__(self,_index=-1,_value=None):
-        self.index=_index
-        self.value=_value
-
-class Row(Field):
-     def __init__(self,_line=None,_delimit="\t"):
-          self.content=[]
-          self.__set(_line,_delimit)
-     def __set(self,line,delimit):
-          try:
-               fields=line.split(delimit)
-               for i, value in enumerate(fields):
-                    self.content.append(Field(i,value))
-          except:
-               print("ERROR in line: %\s \n"%(line))
-
-class FileDOM(Row):
-     def __init__(self,_file_name):
-          self.file_name=_file_name
-          ## header is an object of type rowdom
+class DataMatrixDOM(FileDOM):
+     def __init__(self,_input_files=None,_target_column=-1,_stat_column=-1,_output_dir=None,_output_prefix=None,_round_stat=False):
+          self.files_list=None
+          ## header is an object of type row
           self.header=None
           self.lines_count=0
           self.__set()
